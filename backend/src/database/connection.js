a70ddf0c42a6d559338d36c8ca6f613972e5403c
Isabela Ilma Gonçalves <isabela.ilma@hotmail.com>
2020-05-18 12:55:37 -0300

    Update connection.js

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const configuration = require('../../knexfile');
 
 /* Fazendo a conexão com o banco de teste */
-const config = process.env.NODE_ENV == 'test' ? configuration.test : configuration.development;
+const config = process.env.NODE_ENV || 'test' ? configuration.test : configuration.development;
 
 /* Fazendo a conexão com o banco de dados de desenvolvimento */
 const connection = knex(config);
