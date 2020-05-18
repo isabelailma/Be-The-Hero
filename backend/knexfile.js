0113d1c7ac178520a03a349c761c8ee8556e352c
Isabela Ilma Gonçalves <isabela.ilma@hotmail.com>
2020-05-18 12:10:50 -0300

    Change staging of knexfile.js

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -26,19 +26,10 @@ module.exports = {
 
   staging: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+      tableName: './src/database/migrations'
+    },
 
   production: {
     client: 'postgresql',
